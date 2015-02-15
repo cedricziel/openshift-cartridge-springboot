d683f58f2a9e7a2da4d5d4f149ba29f7fe015524
Cedric Ziel <cedric@cedric-ziel.com>
2015-02-15 16:50:49 +0100

    [TASK] Add http endpoint to boilerplate

--- template/src/main/java/demo/DemoApplication.java
+++ template/src/main/java/demo/DemoApplication.java
@@ -1,12 +1,21 @@
 package demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@Controller
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
     }
 }
