f0a0a08eb072d2d3146d7c69899ea973c38a7186
Cedric Ziel <cedric@cedric-ziel.com>
2015-02-15 21:05:19 +0100

    [TASK] Update Ports for boilerplate code

--- template/src/test/java/demo/DemoApplicationTests.java
+++ template/src/test/java/demo/DemoApplicationTests.java
@@ -4,11 +4,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.IntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = DemoApplication.class)
 @WebAppConfiguration
+@IntegrationTest("server.port:15023")
 public class DemoApplicationTests {
 
 	@Test
